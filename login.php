3496d6f0bbba44749bb70f719616fae91fbc0f98
laghrissi reda <laghrissi.reda123@gmail.com>
2022-01-23 03:55:26 +0100

    new files

--- login.php
+++ login.php
@@ -18,7 +18,7 @@
             $_SESSION['password']=$_POST['password'];
             header('location:dashboard.php');
         }else{
-            header('location:index.php');
+            header('location:index.html');
         }
     }
 }
